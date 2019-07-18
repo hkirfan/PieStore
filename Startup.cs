dea2faba4b8bc4a5fb099dc3d5460e0a473f3f5a
hkirfan <hkirfan@hsc.wvu.edu>
2019-07-18 10:15:59 -0400

    Task completed!

--- Startup.cs
+++ Startup.cs
@@ -38,6 +38,7 @@ namespace BethanyPieShop
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
